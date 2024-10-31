3b889d453ce4475f441f693e1e8c5093f5b7b493
not-icosahedron <not.icosahedron@protonmail.com>
2024-10-31 11:03:12 +0100

    Queries 10.0.0.0/8 Network using the system's DNS Server

--- Projects/DNS_Query.py
+++ Projects/DNS_Query.py
@@ -1,9 +1,40 @@
 import socket
-import time
 
-def query(ip):
-    output = socket.AddressInfo(ip)
-    return output
+file = open("dns_query.csv", "w")
 
-print("Starting DNS query for 10.0.0.0/8 network...")
+print("starting DNS query for 10.0.0.0/8...")
 
+file.write("IP Address,Hostname\n")
+file.flush()
+
+# Open the file using 'with' to ensure it's properly closed
+#with open("dns_query.txt", "a") as file:
+
+
+def print_reverse_dns(ip, file):
+    try:
+        output =str(ip + ','  + socket.gethostbyaddr(ip)[0])
+        output = output.upper()
+        print(output)
+        file.write(output + "\n")
+        file.flush()
+    except Exception:
+        #output = str(ip) + " | No Response..."
+        #print(output)
+        #file.write(output + '\n')
+        #file.flush()
+        return ip, print(str(ip) + " | " + "No Response...")
+
+def generate_ips():
+    for i in range(255):
+        i += 1
+        return i
+
+
+for ip2 in range(0,256):
+    for ip3 in range(0,256):
+        for ip in range(1,256):
+            query = '10.' + str(ip2) + '.' + str(ip3) + '.' + str(ip)
+            print_reverse_dns(query, file)
+
+file.close()
